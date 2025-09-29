d6c6374a16764a16caed86e6978ebe84a28835aa
Christopher Leidigh <cleidigh@Gmail.com>
2025-09-29 18:50:26 -0400

    html and css tweaks

--- src/Modules/ui.mjs
+++ src/Modules/ui.mjs
@@ -4,7 +4,7 @@ import { strftime } from "./strftime.mjs";
 
 export async function createExportStatusWindow(title) {
   //await browser.windows.create({url: "/UI/expStatus.html", type: "popup", top: 200, left: 200, width: 230, height: 180});
-  await browser.windows.create({url: "/UI/expStatus.html", type: "popup", width: 230, height: 180});
+  await browser.windows.create({url: "/UI/expStatus.html", type: "popup", width: 380, height: 240});
 
   //await browser.windows.create({url: "/UI/expStatus.html", type: "popup", top: 200, left: 200});
 
